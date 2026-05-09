8f7dbda714916cb3afa31b0d41fb8c169d6680fb
khanhduygithub <truongnguyenkhanhduy2010pr@gmail.com>
2026-05-09 21:20:02 +0700

    Create list_keys.php

---
+++ api/list_keys.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+$db = json_decode(file_get_contents(__DIR__ . '/database.json'), true);
+echo json_encode(['keys' => $db['keys']]);
